1016d0dd1ed071037d7838df96f6ad594bdffee5
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-12-07 23:13:50 +0500

    un commit link

--- resources/views/layouts/backend/authmaster.blade.php
+++ resources/views/layouts/backend/authmaster.blade.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8" />
         <title>Login | {{config('app.name')}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="{{config('app.name')}}" name="description" />
         <meta content="wasitmirani" name="author" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
--- resources/views/layouts/backend/master.blade.php
+++ resources/views/layouts/backend/master.blade.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <title>Dashboard |{{config('app.name')}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
+        <meta content="{{config('app.name')}}" name="description">
         <meta content="{{config('app.name')}}" name="author">
         <!-- App favicon -->
         <link rel="shortcut icon" href="assets/images/favicon.ico">
