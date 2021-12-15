1133c4807af14740847f398031fa8ba9ce25d59d
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-12-15 21:11:22 +0500

    update mail

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="{{asset('frontend/assets/css/AdminLTE.min.css')}}">
     <title>{{config('app.name')}}</title>
-    <livewire:styles/>
+   @livewireStyles
   </head>
   <body>
 
@@ -27,7 +27,7 @@
 
     <!-- Footer -->
     @include('layouts.frontend.components.footer')
-   <livewire:scripts />
+  @livewireScripts
     <!-- Footer -->
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
