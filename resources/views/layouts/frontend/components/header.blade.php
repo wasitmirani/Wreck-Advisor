b3a9ef56a0979862a68718d4200b92c2eb8f367e
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-12-29 23:11:50 +0500

    update content

--- resources/views/layouts/frontend/components/header.blade.php
+++ resources/views/layouts/frontend/components/header.blade.php
@@ -10,7 +10,7 @@
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                   <li class="nav-item active">
-                    <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only"></span></a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="{{route('about')}}">About Us</a>
