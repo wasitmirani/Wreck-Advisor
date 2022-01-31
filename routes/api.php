7527570b2339e4808745242d952d35e5f5eacee3
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2022-01-31 22:29:59 +0500

    update bareer token

--- routes/api.php
+++ routes/api.php
@@ -40,15 +40,15 @@ Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
     Route::resource('service', ServiceController::class);
     Route::resource('listing', ListingController::class);
 
-    Route::prefix('app')->group(function () {
-        Route::post('/login', [UserController::class, 'login']);
-        Route::post('/store/listing', [ListingController::class, 'storeListing']);
-        Route::get('/listings', [ListingController::class, 'getListings']);
-        Route::post('/register', [UserController::class, 'register']);
-    });
+
 });
 
-
+Route::middleware('auth:sanctum')->prefix('app')->group(function () {
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/store/listing', [ListingController::class, 'storeListing']);
+    Route::get('/listings', [ListingController::class, 'getListings']);
+    Route::post('/register', [UserController::class, 'register']);
+});
 
 
 
